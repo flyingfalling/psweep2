8d75dc70c6e626c03c0e524c033cbbefd4fc7263
Richard Veale <richard.e.veale@gmail.com>
2016-02-21 23:55:33 +0900

    REV: fixed things back to normal case...

--- src/include/dream_abc.h
+++ src/include/dream_abc.h
@@ -106,7 +106,7 @@ struct dream_abc_state
 		 const std::vector<float64_t>& observation_stats,
 		 float64_t epsil=0.05,
 		 int64_t maxgens=1e5,
-		 int64_t numchains=40,
+		 int64_t numchains=100,
 		 int64_t ndelta=3,
 		 float64_t bnoise=0.025,
 		 float64_t bstar=1e-6,
@@ -114,7 +114,7 @@ struct dream_abc_state
 		 int64_t GRskip=50,
 		 int64_t nCR=3,
 		 int64_t pCRskip=10,
-		 float64_t pjump=0.25
+		 float64_t pjump=0.05
 		 )
   {
     state.new_collection( statefilename );
