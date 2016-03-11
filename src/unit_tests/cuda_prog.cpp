4145d286e80d96937688c1efbed3c661e118fe9f
Richard Veale <richard.e.veale@gmail.com>
2016-03-11 21:01:12 +0900

    REV: what. Cuda?

---
+++ src/unit_tests/cuda_prog.cpp
@@ -0,0 +1,44 @@
+
+#include <unit_tests/cuda_prog.h>
+#include <cuda.h>
+
+std::vector<size_t> find_legaldevs()
+{
+  std::vector<size_t> ret;
+
+  
+  int deviceCount = 0;
+  cudaError_t error_id = cudaGetDeviceCount(&deviceCount);
+  if (error_id != cudaSuccess)
+    {
+      printf("cudaGetDeviceCount returned %d\n-> %s\n", (int)error_id, cudaGetErrorString(error_id));
+      printf("Result = FAIL\n");
+      exit(EXIT_FAILURE);
+    }
+  
+  int driverVersion = 0;
+  int runtimeVersion = 0;
+
+  for (int dev = 0; dev < deviceCount; ++dev)
+    {
+      cudaSetDevice(dev);
+      cudaDeviceProp deviceProp;
+      cudaGetDeviceProperties(&deviceProp, dev);
+      /*printf("\nDevice %d: \"%s\"\n", dev, deviceProp.name);
+	char msg[256];
+        SPRINTF(msg, "  Total amount of global memory:                 %.0f MBytes (%llu bytes)\n",
+	(float)deviceProp.totalGlobalMem/1048576.0f, (unsigned long long) deviceProp.totalGlobalMem);
+        printf("%s", msg);
+	printf("  (%2d) Multiprocessors, (%3d) CUDA Cores/MP:     %d CUDA Cores\n",
+	deviceProp.multiProcessorCount,
+	_ConvertSMVer2Cores(deviceProp.major, deviceProp.minor),
+	_ConvertSMVer2Cores(deviceProp.major, deviceProp.minor) * deviceProp.multiProcessorCount);
+      */
+      if( std::string(deviceProp.name).compare( "K80" ) == 0 )
+	{
+	  ret.push_back( dev );
+	}
+    }
+  //cudaDeviceReset();
+  return ret;
+}
