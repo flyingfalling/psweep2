dad50f3b5bcba88e60908620a443e92ac2be7719
Richard Veale <richard.e.veale@gmail.com>
2016-03-15 21:55:52 +0900

    REV: I think everything owrks

--- src/utilscripts/startcudamps.sh
+++ src/utilscripts/startcudamps.sh
@@ -1,3 +1,7 @@
+#ALl GPU must be in exclusive process ?
 
+export CUDA_MPS_PIPE_DIRECTORY=/tmp/nvidia-mps
+export CUDA_MPS_LOG_DIRECTORY=/tmp/nvidia-log
+nvidia-cuda-mps-control -d
 #REV: Apparently I'm supposed to also set visible CPUs etc.?
-sudo nvidia-cuda-mps-control -d
+#sudo nvidia-cuda-mps-control -d
