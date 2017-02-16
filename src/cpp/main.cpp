d2319e8ee452ad083cf2540e9d4746010978c7e5
Richard Veale <richard.e.veale@gmail.com>
2017-02-16 13:13:48 +0900

    REV: aww yiss

---
+++ src/cpp/main.cpp
@@ -0,0 +1,12 @@
+//REV: just runs with the most minimal psweep...
+
+#include <psweep2.h>
+
+int main( int argc, char* argv[] )
+{
+  psweep2 p;
+  
+  p.run_search( argc, argv );
+  
+  return 0;
+}
